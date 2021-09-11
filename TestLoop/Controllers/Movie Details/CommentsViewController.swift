b5d2c0ff5ad822a8bb57535726dbefcf70aa0572
Ahmed Abdelmoula <ahmed.abdelmoula1@esprit.tn>
2021-09-11 11:47:28 +0200

    code formatting

--- TestLoop/Controllers/Movie Details/CommentsViewController.swift
+++ TestLoop/Controllers/Movie Details/CommentsViewController.swift
@@ -14,7 +14,6 @@ class CommentsViewController: UIViewController {
     
     let emptyAnimation = AnimationView(name: "empty_list")
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
